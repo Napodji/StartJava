8dd69de7d4a80c918a4d0509fd3309963cd378bd
Aleks <sasha.k7@mail.ru>
2025-06-30 22:30:22 +0300

    homework/corrected

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -5,23 +5,24 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("\n 1.ПЕРЕВОД ПСЕВДОКОДА НА ЯЗЫК JAVA");
         boolean male = false;
-        int age = 25;
-        double height = 1.40;
         if (!male) {
             System.out.println("Здорово,Чувак!");
         } else {
             System.out.println("Женщинам вход запрещен!");
         }
+        int age = 25;
         if (age > 18) {
             System.out.println("Давай выпьем!");
         } else {
             System.out.println("Иди домой, малец!");
         }
+        double height = 1.40;
         if (height < 1.8) {
             System.out.println("Где потерял кольцо, Фродо?");
         } else {
             System.out.println("Баскетболист что ли?:");
         }
+
         System.out.println("\n 2.ПОИСК БОЛЬШЕГО ЧИСЛА ШАГОВ");
         int yesterdaySteps = 5541;
         int todaySteps = 7537;
@@ -56,129 +57,139 @@ public class IfElseStatementTheme {
         System.out.println("первый способ:");
         String name = "Napodji";
         char firstChar = name.charAt(0);
+        String baseMessage = "Имя " + name + " начинается с ";
+        String result;
         if (firstChar >= 'a' && firstChar <= 'z') {
-            System.out.println("Имя " + name + " начинается с маленькой буквы '" + firstChar + "'");
+            result = baseMessage + "маленькой буквы '" + firstChar + "'";
         } else if (firstChar >= 'A' && firstChar <= 'Z') {
-            System.out.println("Имя " + name + " начинается с большой буквы '" + firstChar + "'");
+            result = baseMessage + "большой буквы '" + firstChar + "'";
         } else if (firstChar >= '0' && firstChar <= '9') {
-            System.out.println("Имя " + name + " начинается с цифры '" + firstChar + "'");
+            result = baseMessage + "цифры '" + firstChar + "'";
         } else {
-            System.out.println("Имя " + name + " начинается с символа '" + firstChar + "'");
+            result = baseMessage + "символа '" + firstChar + "'";
         }
+        System.out.println(result);
+
         System.out.println("второй способ:");
+        String result2;
         if (Character.isLowerCase(firstChar)) {
-            System.out.println("Имя " + name + " начинается с маленькой буквы '" + firstChar + "'");
+            result2 = baseMessage + "маленькой буквы '" + firstChar + "'";
         } else if (Character.isUpperCase(firstChar)) {
-            System.out.println("Имя " + name + " начинается с большой буквы '" + firstChar + "'");
+            result2 = baseMessage + "большой буквы '" + firstChar + "'";
         } else if (Character.isDigit(firstChar)) {
-            System.out.println("Имя " + name + " начинается с цифры '" + firstChar + "'");
+            result2 = baseMessage + "цифры '" + firstChar + "'";
         } else {
-            System.out.println("Имя " + name + " начинается с символа '" + firstChar + "'");
+            result2 = baseMessage + "символа '" + firstChar + "'";
         }
+        System.out.println(result2);
+
         System.out.println("\n 5.ИНВЕНТАРИЗАЦИЯ");
-        int dataBaseNumber = 223;
+        int dbNumber = 223;
         int deviceNumber = 123;
-        String dataBaseNumberStr = String.valueOf(dataBaseNumber);
-        String deviceNumberStr = String.valueOf(deviceNumber);
-        if ((dataBaseNumberStr.charAt(0) == deviceNumberStr.charAt(0)) &&
-                (dataBaseNumberStr.charAt(1) == deviceNumberStr.charAt(1)) &&
-                (dataBaseNumberStr.charAt(2) == deviceNumberStr.charAt(2))) {
+        int dbDigit1 = dbNumber / 100;
+        int dbDigit2 = (dbNumber / 10) % 10;
+        int dbDigit3 = dbNumber % 10;
+        int devDigit1 = deviceNumber / 100;
+        int devDigit2 = (deviceNumber / 10) % 10;
+        int devDigit3 = deviceNumber % 10;
+        if (dbDigit1 == devDigit1 && dbDigit2 == devDigit2 && dbDigit3 == devDigit3) {
             System.out.println("№" + deviceNumber +
-                    ": компьютер на " +
-                    deviceNumberStr.charAt(2) +
-                    " этаже в кабинете №" +
-                    deviceNumberStr.charAt(1));
-        } else if ((dataBaseNumberStr.charAt(0) == deviceNumberStr.charAt(0)) ||
-                (dataBaseNumberStr.charAt(1) == deviceNumberStr.charAt(1)) ||
-                (dataBaseNumberStr.charAt(2) == deviceNumberStr.charAt(2))) {
+                    ": компьютер на " + devDigit3 +
+                    " этаже в кабинете №" + devDigit2);
+        } else if (dbDigit1 == devDigit1 || dbDigit2 == devDigit2 || dbDigit3 == devDigit3) {
+            String devStr = "№";
+            if (dbDigit1 != devDigit1) devStr += "_";
+            else devStr += devDigit1;
+            if (dbDigit2 != devDigit2) devStr += "_";
+            else devStr += devDigit2;
+            if (dbDigit3 != devDigit3) devStr += "_";
+            else devStr += devDigit3;
+            String dbStr = "№" + dbDigit1 + dbDigit2 + dbDigit3;
             System.out.printf("""
                     Нет полного совпадения:
                     База данных: %s
                     Фактический: %s
-                    """, dataBaseNumberStr, deviceNumberStr);
-        } else {
-            System.out.println("№" + deviceNumber + ": оборудование не идентифицировано");
-        }
+                    """, dbStr, devStr);
+        } else System.out.println("№" + deviceNumber + ": оборудование не идентифицировано");
+
         System.out.println("\n 6.ПОДСЧЕТ НАЧИСЛЕННЫХ БАНКОМ %");
         float depositAmount = 321123.79f;
-        float depositRate;
-        float depositRateAmount;
-        float finalDepositAmount;
-        if (depositAmount < 100000) {
-            depositRate = 0.05f;
-        } else if (depositAmount <= 300000) {
+        float depositRate = 0.05f;
+        if (depositAmount >= 100000) {
             depositRate = 0.07f;
-        } else {
-            depositRate = 0.10f;
+            if (depositAmount > 300000) {
+                depositRate = 0.10f;
+            }
         }
-        depositRateAmount = depositAmount * depositRate;
-        finalDepositAmount = depositAmount + depositRateAmount;
+        float depositRateAmount = depositAmount * depositRate;
+        float finalDepositAmount = depositAmount + depositRateAmount;
         System.out.printf("""
                 Первый способ:
                 Сумма вклада: %.2f руб.
                 Сумма начисленного %%: %.2f руб.
                 Итоговая сумма: %.2f руб.\n
                 """, depositAmount, depositRateAmount, finalDepositAmount);
+
         BigDecimal depositAmountBd = new BigDecimal("321123.79");
-        BigDecimal depositRateBd;
-        BigDecimal depositRateAmountBd;
-        BigDecimal finalDepositAmountBd;
-        if (depositAmountBd.compareTo(new BigDecimal(100000)) < 0) {
-            depositRateBd = new BigDecimal("0.05");
-        } else if (depositAmountBd.compareTo(new BigDecimal(300000)) <= 0) {
+        BigDecimal deposit1 = BigDecimal.valueOf(100000);
+        BigDecimal deposit2 = BigDecimal.valueOf(300000);
+        BigDecimal depositRateBd = new BigDecimal("0.05");
+        if (depositAmountBd.compareTo(deposit1) >= 0) {
             depositRateBd = new BigDecimal("0.07");
-        } else {
-            depositRateBd = new BigDecimal("0.10");
+            if (depositAmountBd.compareTo(deposit2) > 0) {
+                depositRateBd = new BigDecimal("0.10");
+            }
         }
-        depositRateAmountBd = depositAmountBd.multiply(depositRateBd).setScale(2, RoundingMode.HALF_UP);
-        finalDepositAmountBd = depositAmountBd.add(depositRateAmountBd).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal depositRateAmountBd = depositAmountBd
+                .multiply(depositRateBd)
+                .setScale(2, RoundingMode.HALF_UP);
+        BigDecimal finalDepositAmountBd = depositAmountBd
+                .add(depositRateAmountBd)
+                .setScale(2, RoundingMode.HALF_UP);
         System.out.printf("""
                 Второй способ:
                 Сумма вклада: %.2f руб.
                 Сумма начисленного %%: %.2f руб.
                 Итоговая сумма: %.2f руб.
-                """, depositAmountBd, depositAmountBd, finalDepositAmountBd);
+                """, depositAmountBd, depositRateAmountBd, finalDepositAmountBd);
+
         System.out.println("\n 7.ОПРЕДЕЛЕНИЕ ОЦЕНКИ ПО ПРЕДМЕТАМ");
-        float historyPoints = 59f;
-        float itPoints = 92f;
-        float historyGrade;
-        if (historyPoints > 91) {
-            historyGrade = 5f;
-        } else if (historyPoints > 73) {
-            historyGrade = 4f;
-        } else if (historyPoints >= 60) {
-            historyGrade = 3f;
-        } else {
-            historyGrade = 2f;
+        int historyPercent = 59;
+        int historyGrade = 2;
+        if (historyPercent > 91) {
+            historyGrade = 5;
+        } else if (historyPercent > 73) {
+            historyGrade = 4;
+        } else if (historyPercent >= 60) {
+            historyGrade = 3;
         }
-        float itGrade;
-        if (itPoints > 91) {
-            itGrade = 5f;
-        } else if (itPoints > 73) {
-            itGrade = 4f;
-        } else if (itPoints >= 60) {
-            itGrade = 3f;
-        } else {
-            itGrade = 2f;
+        int csPercent = 92;
+        int csGrade = 2;
+        if (csPercent > 91) {
+            csGrade = 5;
+        } else if (csPercent > 73) {
+            csGrade = 4;
+        } else if (csPercent >= 60) {
+            csGrade = 3;
         }
-        float averagePoints = (historyPoints + itPoints) / 2;
-        float averageGrade = (historyGrade + itGrade) / 2;
+        double averagePoints = (historyPercent + csPercent) / 2.0;
+        double averageGrade = (historyGrade + csGrade) / 2.0;
         System.out.printf("""
                 История - %d
                 Программирование - %d
                 Средний балл оценок по предметам - %.2f
                 Средний %% по предметам - %.2f
-                """, (int) historyGrade, (int) itGrade, averageGrade, averagePoints);
+                """, historyGrade, csGrade, averageGrade, averagePoints);
+
         System.out.println("\n 8.РАСЧЕТ ГОДОВОЙ ПРИБЫЛИ");
         BigDecimal monthlyProfit = new BigDecimal("13025.233");
         BigDecimal rentCost = new BigDecimal("5123.018");
         BigDecimal selfCost = new BigDecimal("9001.729");
-        BigDecimal monthlyCost;
-        BigDecimal monthlyFinalProfit;
-        BigDecimal yearProfit;
-        monthlyCost = rentCost.add(selfCost);
-        monthlyFinalProfit = monthlyProfit.subtract(monthlyCost);
-        yearProfit = monthlyFinalProfit.multiply(new BigDecimal("12")).setScale(3, RoundingMode.HALF_UP);
-        System.out.println("Прибыль за год: " + yearProfit);
+        BigDecimal monthlyCost = rentCost.add(selfCost);
+        BigDecimal monthlyFinalProfit = monthlyProfit.subtract(monthlyCost);
+        BigDecimal yearProfit = monthlyFinalProfit.multiply(BigDecimal.valueOf(12))
+                .setScale(3, RoundingMode.HALF_UP);
+        String profitSign = yearProfit.compareTo(BigDecimal.ZERO) > 0 ? "+" : "";
+        System.out.println("Прибыль за год: " + profitSign + yearProfit);
     }
 }
