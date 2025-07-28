4d31c34a2aa40915d069d455d692e7d808e8af05
Napoji <sasha.k7@mail.ru>
2025-07-28 18:48:27 +0300

    updated

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите операцию (+, -, *, /, ^, %): ");
+        char operation = scanner.next().charAt(0);
+        System.out.println("Введите второе число: ");
+        int b = scanner.nextInt();
+        long result = 0;
+        String operationStr = "";
+        if (operation == '+') {
+            result = a + b;
+            operationStr = "+";
+        } else if (operation == '-') {
+            result = a - b;
+            operationStr = "-";
+        } else if (operation == '*') {
+            result = a * b;
+            operationStr = "*";
+        } else if (operation == '/') {
+            result = a / b;
+            operationStr = "/";
+        } else if (operation == '^') {
+            result = 1;
+            for (int i = 0; i < b; i++) {
+                result *= a;
+            }
+            operationStr = "^";
+        } else {
+            result = a % b;
+            operationStr = "%";
+        }
+        System.out.println(a + " " + operationStr + " " + b + " = " + result);
+    }
+}
---
+++ Lesson 1/MyFirstGame.java
@@ -0,0 +1,4 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        
+    }
